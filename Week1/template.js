452fbad48cff70284efb0c5379a3d2385153b9bb
jiwonman <kusa1230@naver.com>
2021-12-30 00:54:48 +0900

    2021/12/30 Week1 ppt 완료

---
+++ Week1/template.js
@@ -0,0 +1,12 @@
+let name = 'jiwon';
+let letter = 'Hello ' + name + '\n\n' + name;
+console.log(letter);
+
+console.log('-----------');
+
+letter = `Hi ${name} 
+
+Nice to meet you ${name}
+${1+1}`;
+
+console.log(letter);
