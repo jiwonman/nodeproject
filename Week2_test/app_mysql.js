8d90131f3319b9f01a526d545b3ace8c4102c991
jiwonman <kusa1230@naver.com>
2022-01-10 15:53:20 +0900

    CRUD 완료

--- Week2_test/app_mysql.js
+++ Week2_test/app_mysql.js
@@ -30,6 +30,88 @@ app.get('/topic/add', (req, res) => {
     })
 });
 
+app.get(['/topic/:id/edit'], (req, res) => {
+    let sql = 'select id, title from topic';            //글 본문을 출력하기 위함.
+    conn.query(sql, (err, topics, fields) => {
+        if(err){
+            console.log(err);
+        } else {
+            const id = req.params.id;
+            if(id){
+                sql = 'select * from topic where id=?';
+                conn.query(sql, [id], (err, topic, fields) => {
+                    if(err){
+                        console.log(err);
+                        res.status(500).send('Internal Sever Error');
+                    } else{
+                        res.render('edit', {topics:topics, topic:topic[0], id:id});
+                    }
+                })
+            }
+            else{
+                console.log('There is no id');
+                res.status(500).send('Internal Sever Error');
+            }
+        }
+    })
+});
+
+app.post(['/topic/:id/edit'], (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const author = req.body.author;
+    const id = req.params.id;
+    let sql = 'update topic set title=?, description=?, author=? where id=?';           
+    conn.query(sql, [title, description, author, id], (err, result, fields) => {
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Sever Error');
+        } else {
+            res.redirect('/topic/' + id);
+        }
+    })
+});
+
+app.get(['/topic/:id/delete'], (req, res) => {
+    let sql = 'select id, title from topic';
+    const id = req.params.id;
+    conn.query(sql, (err, topics, fields) => {
+        if(err){
+            console.log(err);
+        }
+        else {
+            sql = 'select * from topic where id=?';
+            conn.query(sql, [id], (err, topic, fields) => {
+                if(err){
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                }   else {
+                    if(topic.length === 0){
+                        console.log('There is no id');
+                        res.status(500).send('Internal Server Error');
+                    } else {
+                        res.render('delete', {topics:topics, topic:topic[0]});
+                    }
+                }
+            })
+        }
+    })
+});
+
+app.post('/topic/:id/delete', (req, res) => {
+    const id = req.params.id;
+    let sql = 'delete from topic where id=?';
+    conn.query(sql, [id], (err, result, fields) => {
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }   else {
+            res.redirect('/topic/');
+            // res.redirect('/topic/' + result.insertId);
+        }
+    })
+});
+
 app.post('/topic/add', (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
